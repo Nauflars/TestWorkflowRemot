cbe8eb877226a9c95894e2b560430f8b75576fe6
Nauflars <naoufal.lars@gmail.com>
2019-04-30 20:07:13 +0200

    commit 2 usuario 2

--- BlogController.php
+++ BlogController.php
@@ -101,4 +101,18 @@ class BlogController extends AbstractController
 
         // ...
     }
+
+
+    /**
+     * Matches /blog7/*
+     *
+     * @Route("/blog/{slug}", name="blog_show")
+     */
+    public function show7($slug)
+    {
+        // $slug will equal the dynamic part of the URL
+        // e.g. at /blog/yay-routing, then $slug='yay-routing'
+
+        // ...
+    }
 }
