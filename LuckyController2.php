bbd03d863c818d3c296d94aae3ae40504f440d42
Nauflars <naoufal.lars@gmail.com>
2019-05-01 23:46:31 +0200

    commit branch 2

--- LuckyController2.php
+++ LuckyController2.php
@@ -282,32 +282,7 @@ class LuckyController extends AbstractController
     }
 
 
-    /**
-     * @Route("/lucky/number8")
-     */
-    public function number14()
-    {
-        $number = random_int(0, 1000);
-        if ($number > 50)
-        {
-            $number=50;
-        }else if ($number>20)
-        {
-            $number=0;
-        }else if ($number<20){
-            $number=20;
-        }else if ($number<20)
-        {
-            $number=30;
-        }else if ($number>0)
-        {
-            $number=0;
-        }
 
-        return $this->render('lucky/number.html.twig', [
-            'number' => $number,
-        ]);
-    }
     /**
      * @Route("/lucky/number8")
      */
